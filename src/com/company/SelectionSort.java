aa3356d039c51cf1b2e3b3786e0f5baac39eb17c
isaacmcoe <isaacmcoe73@gmail.com>
2019-05-05 16:53:40 -0400

    first commit

---
+++ src/com/company/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.company;
+public class SelectionSort
+{
+    public static void selectionSort(int[] arr)
+    {
+        //outer loop controlling the index position
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            //tying index to outer loop
+            int index = i;
+            //loop ranking current index against current min
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (arr[j] < arr[index])
+                {
+                    //new min
+                    index = j;
+                }
+            }
+            int smallerNumber = arr[index];
+            //sorting index into lowest available position
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+    }
+}
